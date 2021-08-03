27018d155139727acf2423a6276c6baef988bfc2
ryuji.tamakawa <homes31285@gmail.com>
2021-08-03 11:54:44 +0000

    second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,21 @@
 #メソッドの定義
+
 def fizzbuzz(num)
-    if (num % 3 == 0) && (num % 5 != 0)
-        puts 'Fizz'
-    elsif (num % 3 != 0) && (num % 5 == 0)
-        puts 'Buzz'
-    elsif (num % 3 == 0) && (num % 5 == 0)
-        puts 'FizzBuzz'
-    else 
-        puts num
+        if (num % 3 == 0) && (num % 5 != 0)
+            return 'Fizz'
+        elsif (num % 3 != 0) && (num % 5 == 0)
+            return 'Buzz'
+        elsif (num % 3 == 0) && (num % 5 == 0)
+            return 'FizzBuzz'
+        else 
+            return num
+        end
     end
+#メソッドを呼び出し
+    num_max = 100
+    (1..num_max).each do |num|
+    puts fizzbuzz(num)
 end
 
-#メソッドの実行
-num_max = 100
-(1..num_max).each do |num|
-     fizzbuzz(num)
-end
+
+
